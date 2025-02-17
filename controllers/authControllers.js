c90b148c010b0130c7d850ea199e0fb52570f37b
Lev <lev.bereza@gmail.com>
2025-02-17 17:45:00 +0200

    Checking if passwords match

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,9 +4,23 @@ const Customer = require('../models/Customer');
 const handleError = require('../util/handleError');
 
 const signUp = async (req, res, next) => {
-  const { customer, email, contactName, address, phone, password } = req.body;
+  const {
+    customer,
+    email,
+    contactName,
+    address,
+    phone,
+    password,
+    confPassword,
+  } = req.body;
 
   try {
+    if (password !== confPassword) {
+      const error = new Error('Passwords do not match');
+      error.statusCode = 400;
+      return next(error);
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const newCustomer = await Customer.create({
       customer: customer,
