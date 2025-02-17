db1ae6fdd3519ca9d7947a3fd912e79ed07ea3be
Lev <lev.bereza@gmail.com>
2025-02-17 12:13:49 +0200

    Getting single product

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -12,4 +12,23 @@ const getProducts = async (req, res, next) => {
   }
 };
 
-module.exports = { getProducts };
+const getProduct = async (req, res, next) => {
+  const productId = req.params.id;
+
+  try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      const error = new Error('Failed to find post with id ' + product);
+      error.statusCode = 404;
+      return next(error);
+    }
+    res.status(200).json({
+      message: 'Fetched post successfully',
+      product: product,
+    });
+  } catch (error) {
+    handleError(error, next);
+  }
+};
+
+module.exports = { getProducts, getProduct };
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const mainControllers = require('../controllers/mainControllers');
 
 router.get('/products', mainControllers.getProducts);
+router.get('/products/:id', mainControllers.getProduct);
 
 module.exports = router;
